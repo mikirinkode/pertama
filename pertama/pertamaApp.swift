a42125a340a41fc2f2ae3bb5fb19f61185a04b94
wafa <wafa.appdev@gmail.com>
2024-11-09 10:29:27 +0700

    Initial Commit

---
+++ pertama/pertamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pertamaApp.swift
+//  pertama
+//
+//  Created by MacBook on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct pertamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pertamaTests/pertamaTests.swift
@@ -0,0 +1,16 @@
+//
+//  pertamaTests.swift
+//  pertamaTests
+//
+//  Created by MacBook on 09/11/24.
+//
+
+import Testing
+
+struct pertamaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
